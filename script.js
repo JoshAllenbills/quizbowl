8b70595f386e807e26a501e66659f7ea7a68aa0a
Colin <colind2824@tuckahoeschools.org>
2024-11-12 13:12:23 +0000

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const submitButton = document.querySelector('.submit');
 const header = document.querySelector('.header');
-const score = document.querySelector('.score');
+const score = score += 1; document.querySelector('.score').textContent = score;
 const questions = document.querySelector('.questions');
 const inputContainer = document.querySelector('.input-container');
 const outputContainer = document.querySelector('.output-container');
@@ -12,10 +12,17 @@ submitSwitch.addEventListener("click", () => {
     document.querySelector('.questions').textContent = questionsArray[currentQuestionIndex];
 });
 
+
+
 const questionsArray = [
     "How old is ben?",
     "What is 404 x 30 / 5?",
     "What is my first name backwards?"
 ];
 
-let currentQuestionIndex = 0;
+const answersArray = [
+    "5",
+    "3232",
+    "niloc"
+];
+document.querySelector('.questions').textContent = questionsArray[currentQuestionIndex];
