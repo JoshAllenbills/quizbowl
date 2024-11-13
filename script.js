ded783f5528f9592c780606690bd2f26e491c8d8
Colin <colind2824@tuckahoeschools.org>
2024-11-13 13:06:16 +0000

    initial commit

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 const submitButton = document.querySelector('.submit');
 const header = document.querySelector('.header');
-const score = document.querySelector('.score');
+const scoreContainer = document.querySelector('.score');
 const questions = document.querySelector('.questions');
-const inputContainer = document.querySelector('.input-container');
+const input = document.querySelector('.input-container');
 const outputContainer = document.querySelector('.output-container');
 const submitSwitch = document.getElementById("submit-switch");
-submitSwitch.addEventListener("click", () => {
-    document.getElementById('name').value = '';
+let isFirstSubmission = true;
 
+submitSwitch.addEventListener("click", () => {
+    if (isFirstSubmission) {
+        isFirstSubmission = false;
+    } else {
+        const userAnswer = document.getElementById('input-field').value;
+        if (userAnswer === questionsArray[currentQuestionIndex].answer) {
+            alert("GOOD BOY");
+        } else {
+            alert("BAD BOY");
+        }
+    }
+    document.getElementById('input-field').value = '';
     currentQuestionIndex = (currentQuestionIndex + 1) % questionsArray.length;
-    document.querySelector('.questions').textContent = questionsArray[currentQuestionIndex];
+    document.querySelector('.questions').textContent = questionsArray[currentQuestionIndex].question;
 });
 
 const questionsArray = [
-    "How old is Jeffery?",
-    "What is 938 x 54/ 24?",
-    "What is my last name spelt backwards?",
-    "What is the capital of croatia?",
-    "What is 2 + 24?",
-    "Name a not color.",
-    "What is the largest planet in your solar system?",
-    "What is the square root of 85?",
-    "Who wrote 'To Kill a Mockingbird'?",
-    "What is the boiling point of water in Celsius?"
+    { question: "How old is BEN?", answer: "5" },
+    { question: "What is 938 x 10 / 4?", answer: "2345" },
+    { question: "What is my last name spelt backwards?", answer: "kajnikrujD" },
+    { question: "What is the capital of Croatia?", answer: "Zagreb" },
+    { question: "What is Mr.Cooper's first name?", answer: "Louis" },
+    { question: "This is a Quiz____!", answer: "bowl" },
+    { question: "Is Pluto a planet (in my heart)?", answer: "yes" },
+    { question: "Who can rap faster, A human or AI?", answer: "AI" },
+    { question: "Is the book 'Hunger Games' about hungry people playing some video games?", answer: "no" },
+    { question: "DO YOU LIKE BACON SAY YES NOW", answer: "yes" }
 ];
 
 let currentQuestionIndex = 0;
